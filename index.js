8e32d4445089a1d08c102a0fb43b87f46c57e884
hirarira <hirarira617@gmail.com>
2021-08-07 19:50:41 +0900

    画像の描画までしました

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 "use strict"
-console.log("hoge");
+window.onload = () => {
+  class BaseImage {
+    constructor(ctx) {
+      this.image = new Image();
+      this.image.src = "./image/annica.jpg";
+      this.imageSize = 1080;
+      this.ctx = ctx;
+    }
+    getImage() {
+      return this.image;
+    }
+    drawImage() {
+      this.ctx.drawImage(
+        baseImage.getImage(),
+        0,
+        0,
+        this.imageSize,
+        this.imageSize,
+        0,
+        0,
+        800,
+        800
+      );
+    }
+  }
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const baseImage = new BaseImage(ctx);
+  baseImage.getImage().onload = () => {
+    baseImage.drawImage();
+  }
+}
